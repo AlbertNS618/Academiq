4dc38f4375ca9bfa3ed8d77aea83771da3deab4d
AlbertNS <albertnathan.2003@gmail.com>
2023-12-05 00:07:51 +0700

    add js, update composer

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,9 +4,46 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <link rel="stylesheet" href="{{url('/css/bootstrap.css')}}">
+    <title>Homepage</title>
 </head>
 <body>
-
+    <nav class="navbar navbar-expand-md bg-body-tertiary">
+        <div class="container-fluid">
+            <img src="{{url('/img/logo.png')}}" alt="" width="60px" class="navbar-brand mx-5">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse ms-5">
+                <ul class="navbar-nav">
+                    <li class="nav-item active mx-5">
+                        <a class="nav-link" href="#">Home</a>
+                    </li>
+                    <li class="nav-item mx-5">
+                        <a class="nav-link" href="">Subject</a>
+                    </li>
+                    <li class="nav-item mx-5">
+                        <a class="nav-link" href="">Exercise</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="collapse navbar-collapse">
+                <ul class="navbar-nav">
+                    <li>
+                        <a class="nav-link" href="">Login</a>
+                    </li>
+                    <li>
+                        <a class="nav-link" href="">Register</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <header>
+        <div class="bg-primary">
+            hELLO
+        </div>
+    </header>
+    <script ></script>
 </body>
 </html>
