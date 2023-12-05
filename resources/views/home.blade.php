c5e5b3dc6327414154043fe0eb33be71c175d978
AlbertNS <albertnathan.2003@gmail.com>
2023-12-05 22:41:34 +0700

    more styling

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,11 +5,12 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="{{url('/css/bootstrap.css')}}">
+    <link rel="stylesheet" href="{{url('/css/own-style.css')}}">
     <title>Homepage</title>
 </head>
 <body>
-    <nav class="navbar navbar-expand-md bg-body-tertiary">
-        <div class="container-fluid">
+    <nav class="navbar navbar-expand-md bg-light">
+        <div class="container-fluid fw-medium">
             <img src="{{url('/img/logo.png')}}" alt="" width="60px" class="navbar-brand mx-5">
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
@@ -32,7 +33,7 @@
                     <li>
                         <a class="nav-link" href="">Login</a>
                     </li>
-                    <li>
+                    <li class="">
                         <a class="nav-link" href="">Register</a>
                     </li>
                 </ul>
@@ -40,8 +41,19 @@
         </div>
     </nav>
     <header>
-        <div class="bg-primary">
-            hELLO
+        <div class="bgHeaderColor d-flex align-items-center px-4 py-2 justify-content-between">
+            <div>
+                <h1>Elevate Your Learning</h1>
+                <p class=" w-75">
+                    Begin your learning journey wherever level you're in.
+                    We will help you!
+                </p>
+                <div class="badge rounded-pill bg-warning px-4 py-3 fw-bold fs-4 ">
+                    <a class="text-dark text-decoration-none">Start Now!</a>
+                </div>
+            </div>
+            <img class="rounded-circle bg-white m-3" width="450px" src="{{url('/img/man-header.png')}}" alt="">
+
         </div>
     </header>
     <script ></script>
