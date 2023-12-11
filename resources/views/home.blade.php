dc12af98126344683547d659177f87d2d24aece0
AlbertNS <albertnathan.2003@gmail.com>
2023-12-12 01:09:17 +0700

    deletes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -140,7 +140,6 @@
             </div>
             <div class=" badge badge-pill bg-black">Plan Now <i class="bi bi-arrow-right"></i></div>
             <img src="" alt="">
-            <div class="star-five"></div>
         </div>
     </section>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
