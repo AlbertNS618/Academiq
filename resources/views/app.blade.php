7e7b7ce2e19b2e55f88353045bafcf269cabddc3
AlbertNS <albertnathan.2003@gmail.com>
2023-12-12 21:31:21 +0700

    tambahin icon di tab

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -7,6 +7,7 @@
     {{-- <link rel="stylesheet" href="{{url('/css/bootstrap.css')}}"> --}}
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="{{url('/css/own-style.css')}}">
+    <link rel="icon" href="{{url('/img/logo.png')}}" type="image/icon type">
     <title>@yield('title', $title)</title>
 </head>
 <body>
