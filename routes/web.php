865e0b1851793dcffffe30ed82f6351346bb02b2
AlbertNS <albertnathan.2003@gmail.com>
2023-12-12 21:41:31 +0700

    add subject page

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,4 @@ Route::get('/register', [UserController::class, 'register'])->name('register');
 Route::post('/register', [UserController::class, 'register_action'])->name('register.action');
 Route::get('/login', [UserController::class, 'login'])->name('login');
 Route::post('/login', [UserController::class, 'login_action'])->name('login.action');
+Route::get('/subject', [UserController::class, 'subject'])->name('subject');
